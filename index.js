239856a110610928b08d47c7cedf0e238b9f279d
Gavriel Hass <gavhass@gmail.com>
2017-10-04 17:20:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -7,9 +7,6 @@ function whisper(string) {
   return string.toLowerCase()
 }
 
-function logShout (hello){
-  console.log ('${hello}')
-}
 
 function logShout(string) {
    console.log(string.toUpperCase()
