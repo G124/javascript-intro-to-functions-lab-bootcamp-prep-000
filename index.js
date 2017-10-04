64a02436a9b93be5b2ea2b49a64d5504ddf2dbe1
Gavriel Hass <gavhass@gmail.com>
2017-10-04 17:58:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,5 +19,5 @@ function logShout(string) {
 
  function sayHiToGrandma(string){
    if (string.toLowerCase()===string){
-        return "I can't hear you!"
+        return "I can/t hear you!"
       }
